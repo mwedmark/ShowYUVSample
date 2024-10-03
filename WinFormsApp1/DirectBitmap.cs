03835e57aa832690906efa90bb88e6724fbeb424
magwed <128053140+magwed@users.noreply.github.com>
2024-10-03 10:05:26 +0200

    Updated solution

--- WinFormsApp1/DirectBitmap.cs
+++ WinFormsApp1/DirectBitmap.cs
@@ -1,10 +1,12 @@
 ﻿using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 
+namespace WinFormsApp1;
+
 public class DirectBitmap : IDisposable
 {
     public Bitmap Bitmap { get; private set; }
-    public Int32[] Bits { get; private set; }
+    public int[] Bits { get; private set; }
     public bool Disposed { get; private set; }
     public int Height { get; private set; }
     public int Width { get; private set; }
@@ -15,24 +17,24 @@ public class DirectBitmap : IDisposable
     {
         Width = width;
         Height = height;
-        Bits = new Int32[width * height];
+        Bits = new int[width * height];
         BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
         Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
     }
 
-    public void SetPixel(int x, int y, Color colour)
+    public void SetPixel(int x, int y, Color color)
     {
-        int index = x + (y * Width);
-        int col = colour.ToArgb();
+        var index = x + (y * Width);
+        var col = color.ToArgb();
 
         Bits[index] = col;
     }
 
     public Color GetPixel(int x, int y)
     {
-        int index = x + (y * Width);
-        int col = Bits[index];
-        Color result = Color.FromArgb(col);
+        var index = x + (y * Width);
+        var col = Bits[index];
+        var result = Color.FromArgb(col);
 
         return result;
     }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,193 +1,139 @@
 using System.Drawing.Drawing2D;
 using Timer = System.Windows.Forms.Timer;
 
-namespace WinFormsApp1
+namespace WinFormsApp1;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    private readonly VideoBuffer _videoBuffer = new();
+    private readonly Timer _timer = new();
+
+    public Form1()
     {
-        private const int Xsize = 352; //176
-        private const int Ysize = 288; //144
-        private const int sizeOfBWFrame = Xsize * Ysize;
-        private const int sizeOfFrameComponent = Xsize * Ysize / 4;
-        private const int dataBufferSize = sizeOfBWFrame + sizeOfFrameComponent * 2;
-        private const string filename = "akiyo_cif.yuv";
+        InitializeComponent();
+    }
 
-        private readonly byte[] dataBuffer = new byte[dataBufferSize];
-        private int currentFrame;
-        private Timer timer = new();
-        private DirectBitmap bitmap = new(Xsize, Ysize);
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        SetupTimer();
+        InitializeControls();
+    }
 
-        public Form1()
+    private void SetupTimer()
+    {
+        _timer.Interval = 1000 / (int)nudFrameRate.Value;
+        _timer.Tick += Timer_Tick;
+        _timer.Start();
+    }
+
+    private void InitializeControls()
+    {
+        var interpolationValues = Enum.GetValues<InterpolationMode>().Select(i => i.ToString()).ToList();
+        interpolationValues.Remove("Invalid");
+        cbInterpolationMode.Items.AddRange(interpolationValues.ToArray());
+        cbInterpolationMode.SelectedIndex = 5;
+
+        var compositingQualityValues = Enum.GetValues<CompositingQuality>().Select(i => i.ToString()).ToList();
+        cbCompositingQuality.Items.AddRange(compositingQualityValues.ToArray());
+        cbCompositingQuality.SelectedIndex = 0;
+
+        var compositingModeValues = Enum.GetValues<CompositingMode>().Select(i => i.ToString()).ToList();
+        cbCompositingMode.Items.AddRange(compositingModeValues.ToArray());
+        cbCompositingMode.SelectedIndex = 0;
+
+        var pixelOffsetModeValues = Enum.GetValues<PixelOffsetMode>().Select(i => i.ToString()).ToList();
+        cbPixelOffsetMode.Items.AddRange(pixelOffsetModeValues.ToArray());
+        cbPixelOffsetMode.SelectedIndex = 0;
+
+
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+        _videoBuffer.CurrentFrame++;
+        if (_videoBuffer.CurrentFrame >= 300) _videoBuffer.CurrentFrame = 0;
+        RepaintPicture();
+    }
+
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+        RepaintPicture();
+    }
+
+    private void RepaintPicture()
+    {
+        _videoBuffer.FillBufferWithNewFrame();
+        _videoBuffer.CalculateFrame(checkBox1.Checked);
+        pictureBox1.Image = _videoBuffer.DirectBitmap.Bitmap;
+    }
+       
+    private void checkBox1_CheckedChanged(object sender, EventArgs e)
+    {
+        RepaintPicture();
+    }
+
+    private void Form1_DoubleClick(object sender, EventArgs e)
+    {
+        SwitchModes();
+    }
+
+    private void pictureBox1_DoubleClick(object sender, EventArgs e)
+    {
+        SwitchModes();
+    }
+
+    private void SwitchModes()
+    {
+        if (WindowState == FormWindowState.Maximized)
+            SetWindowed();
+        else
+            SetFullScreen();
+    }
+
+    private void SetFullScreen()
+    {
+        _originalStyle = FormBorderStyle;
+        _originalState = WindowState;
+        _originalHeight = Height;
+        _originalWidth = Width;
+
+        FormBorderStyle = FormBorderStyle.None;
+        WindowState = FormWindowState.Maximized;
+    }
+
+    private FormBorderStyle _originalStyle;
+    private FormWindowState _originalState;
+    private int _originalWidth;
+    private int _originalHeight;
+
+    private void SetWindowed()
+    {
+        FormBorderStyle = _originalStyle;
+        WindowState = _originalState;
+        Width = _originalWidth;
+        Height = _originalHeight;
+    }
+
+    private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var combo = sender as ComboBox;
+
+        pictureBox1.InterpolationMode = Enum.Parse<InterpolationMode>(combo.SelectedItem.ToString());
+        BeginInvoke(new Action(() => combo.Select(int.MaxValue, 0)));
+    }
+
+    private void nudFrameRate_ValueChanged(object sender, EventArgs e)
+    {
+        _timer.Interval = 1000 / (int)nudFrameRate.Value;
+    }
+
+    private void nudFrameRate_Leave(object sender, EventArgs e)
+    {
+        var nudControl = sender as NumericUpDown;
+
+        if (nudControl!.Text == "")
         {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            SetupTimer();
-            InitializeControls();
-        }
-
-        private void SetupTimer()
-        {
-            //var fps = 30;
-            timer.Interval = 1000 / (int)nudFrameRate.Value;
-            timer.Tick += Timer_Tick;
-            timer.Start();
-        }
-
-        private void InitializeControls()
-        {
-            // interpolation
-            var interpolationValues = Enum.GetValues<InterpolationMode>().Select(i => i.ToString()).ToList();
-            interpolationValues.Remove("Invalid");
-            cbInterpolationMode.Items.AddRange(interpolationValues.ToArray());
-            cbInterpolationMode.SelectedIndex = 5;
-
-            //public CompositingQuality CompositingQuality { get; set; }
-            var compositingQualityValues = Enum.GetValues<CompositingQuality>().Select(i => i.ToString()).ToList();
-            //interpolationValues.Remove("Invalid");
-            cbCompositingQuality.Items.AddRange(compositingQualityValues.ToArray());
-            cbCompositingQuality.SelectedIndex = 0;
-
-            //public CompositingMode CompositingMode { get; set; }
-            var CompositingModeValues = Enum.GetValues<CompositingMode>().Select(i => i.ToString()).ToList();
-            //interpolationValues.Remove("Invalid");
-            cbCompositingMode.Items.AddRange(CompositingModeValues.ToArray());
-            cbCompositingMode.SelectedIndex = 0;
-
-            //public PixelOffsetMode PixelOffsetMode { get; set; }
-            var PixelOffsetModeValues = Enum.GetValues<PixelOffsetMode>().Select(i => i.ToString()).ToList();
-            //interpolationValues.Remove("Invalid");
-            cbPixelOffsetMode.Items.AddRange(PixelOffsetModeValues.ToArray());
-            cbPixelOffsetMode.SelectedIndex = 0;
-
-
-        }
-
-        private void Timer_Tick(object? sender, EventArgs e)
-        {
-            currentFrame++;
-            if (currentFrame >= 300) currentFrame = 0;
-            RepaintPicture();
-        }
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            RepaintPicture();
-        }
-
-        private void RepaintPicture()
-        {
-            FillBufferWithNewFrame();
-            for (int y = 0; y < Ysize - 1; y++)
-            {
-                for (int x = 0; x < Xsize - 1; x++)
-                {
-                    var Y = dataBuffer[y * Xsize + x];
-                    var currentOffsetInComponents = (y >> 1) * (Xsize >> 1) + (x >> 1);
-                    var currentOffset = sizeOfBWFrame + currentOffsetInComponents;
-                    var u = !checkBox1.Checked ? 0 :
-                        dataBuffer[currentOffset] - 128;
-                    var v = !checkBox1.Checked ? 0 :
-                        dataBuffer[currentOffset + sizeOfFrameComponent] - 128;
-
-                    var r = Cap(Y + ((73 * v) >> 6));
-                    var g = Cap(Y - ((101 * u) >> 8) - ((595 * v) >> 10));
-                    var b = Cap(Y + ((1041 * u) >> 9));
-
-                    bitmap.SetPixel(x, y, Color.FromArgb(r, g, b));
-                }
-            }
-
-            pictureBox1.Image = bitmap.Bitmap;
-        }
-
-        private void FillBufferWithNewFrame()
-        {
-            using var file = File.OpenRead(filename);
-            int bytesRead;
-
-            file.Seek(dataBufferSize * currentFrame, SeekOrigin.Begin);
-            bytesRead = file.Read(dataBuffer, 0, dataBuffer.Length);
-        }
-
-        private int Cap(int input)
-        {
-            if (input < 0) return 0;
-            if (input > 255) return 255;
-
-            return input;
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            RepaintPicture();
-        }
-
-        private void Form1_DoubleClick(object sender, EventArgs e)
-        {
-            switchModes();
-        }
-
-        private void pictureBox1_DoubleClick(object sender, EventArgs e)
-        {
-            switchModes();
-        }
-
-        private void switchModes()
-        {
-            if (WindowState == FormWindowState.Maximized)
-                SetWindowed();
-            else
-                SetFullScreen();
-        }
-
-        private void SetFullScreen()
-        {
-            OriginalStyle = FormBorderStyle;
-            OriginalState = WindowState;
-            OriginalHeight = Height;
-            OriginalWidth = Width;
-
-            FormBorderStyle = FormBorderStyle.None;
-            WindowState = FormWindowState.Maximized;
-        }
-
-        private FormBorderStyle OriginalStyle;
-        private FormWindowState OriginalState;
-        private int OriginalWidth;
-        private int OriginalHeight;
-
-        private void SetWindowed()
-        {
-            FormBorderStyle = OriginalStyle;
-            WindowState = OriginalState;
-            Width = OriginalWidth;
-            Height = OriginalHeight;
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var combo = sender as ComboBox;
-
-            pictureBox1.InterpolationMode = Enum.Parse<InterpolationMode>(combo.SelectedItem.ToString());
-            BeginInvoke(new Action(() => combo.Select(int.MaxValue, 0)));
-        }
-
-        private void nudFrameRate_ValueChanged(object sender, EventArgs e)
-        {
-            timer.Interval = 1000 / (int)nudFrameRate.Value;
-        }
-
-        private void nudFrameRate_Leave(object sender, EventArgs e)
-        {
-            var nudControl = sender as NumericUpDown;
-
-            if (nudControl!.Text == "")
-            {
-                nudControl.Text = "30";
-            }
+            nudControl.Text = "30";
         }
     }
 }
